b17c588a91fa302603a341978ca8035c9f93d2be
TMGabella <tmgabella@gmail.com>
2023-12-23 17:15:56 +0700

    end

--- app/src/main/java/com/example/pervushin_v_5/personal_area.java
+++ app/src/main/java/com/example/pervushin_v_5/personal_area.java
@@ -1,4 +1,48 @@
 package com.example.pervushin_v_5;
 
-public class personal_area {
+
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.Button;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class personal_area extends AppCompatActivity {
+
+    private static final int REQUEST_IMAGE_CAPTURE = 1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.personal_area);
+
+        Button buttonSearchByPhoto = findViewById(R.id.buttonSearchByPhoto);
+
+        buttonSearchByPhoto.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Создание Intent для открытия камеры
+                Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+                    startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+                }
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == Activity.RESULT_OK) {
+            // Обработка снимка с камеры
+            // Здесь вы можете получить изображение из data и выполнить необходимые действия
+        }
+    }
 }
+
+
